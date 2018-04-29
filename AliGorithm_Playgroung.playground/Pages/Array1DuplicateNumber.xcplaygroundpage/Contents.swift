0ac257f01fdec317d48d91c33f1f63e421dd676c
AliGorithm <alisher.coder@gmail.com>
2018-04-29 14:45:29 -0500

    DuplicateCommentUpdated

--- AliGorithm_Playgroung.playground/Pages/Array1DuplicateNumber.xcplaygroundpage/Contents.swift
+++ AliGorithm_Playgroung.playground/Pages/Array1DuplicateNumber.xcplaygroundpage/Contents.swift
@@ -99,15 +99,19 @@ func rotateImage(a: [[Int]]) -> [[Int]] {
 //rotateImage(a: [[1,2,3], [4,5,6], [7,8,9]])
 
 """
-You have two integer arrays, a and b, and an integer target value v.
-Determine whether there is a pair of numbers, where one number is
-taken from a and the other from b, that can be added together
-to get a sum of v. Return true if such a pair exists, otherwise return false.
+Given an array of integers, write a function that determines whether the array
+contains any duplicates. Your function should return true if any element appears
+at least twice in the array, and it should return false if every element is distinct.
 
 Example
 
-For a = [1, 2, 3], b = [10, 20, 30, 40], and v = 42, the output should be
-sumOfTwo(a, b, v) = true
+For a = [1, 2, 3, 1], the output should be
+containsDuplicates(a) = true.
+
+There are two 1s in the given array.
+
+For a = [3, 1], the output should be
+containsDuplicates(a) = false.
 
 """
 
